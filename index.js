f7abf3e1068cf05449318b018e5743640484f7f3
Yasir Saeed <86898726+ykkhan1997@users.noreply.github.com>
2024-01-01 10:24:00 +0300

    eight commit

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const express=require("express");
+const express = require("express");
+const BlockChain = require("./blockchain/index.js");
+const State = require("./store/state.js");
 
-const app=express();
+const state = new State();
+const blockchain = new BlockChain({ state });
+const app = express();
 
 app.use(express.json());
 
-
-app.get('/',(req,res,next)=>{
+app.get("/", (req, res, next) => {
   res.status(200).json("Hello World");
 });
-
+app.get("/blockchain", (req, res, next) => {
+  try {
+    const { chain } = blockchain;
+    res.status(200).json({ chain });
+  } catch(error){
+    next(error);
+  }
+});
+app.use((err, req, res, next) => {
+  console.log(`Internal server error 500 ${err.message}`);
+  res.status(500).json({ message: err.message });
+});
 let PORT;
-let port=PORT || 3000;
+let port = PORT || 3000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Your Port is running on port ${port}`);
 });
